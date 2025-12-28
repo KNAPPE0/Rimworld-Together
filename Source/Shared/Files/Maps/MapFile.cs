abf147c2475a120c80f101d0522f354c177fd088
KNAPPE0 <abrahammonarrez0000@gmail.com>
2025-12-28 02:12:02 -0600

    MapFile Fixes for PR Compat
    Fixes for PR

--- Source/Shared/Files/Maps/MapFile.cs
+++ Source/Shared/Files/Maps/MapFile.cs
@@ -9,9 +9,25 @@ namespace Shared.Files.Maps
 
         public int[] Size { get; set; } = null;
 
+        public string Username { get; set; } = string.Empty;
+
+        public string SettlementName { get; set; } = string.Empty;
+
+        public string FactionName { get; set; } = string.Empty;
+
+        public double WealthExact { get; set; } = -1;
+
+        public int GameTicks { get; set; } = -1;
+
+        public double RealPlayTimeInteractingSeconds { get; set; } = -1;
+
+        public long LastSavedUtcTicks { get; set; } = 0;
+
         public int Wealth { get; set; } = -1;
 
         public byte WeatherByte { get; set; } = byte.MaxValue;
+
+        public string CurWeatherDefName { get; set; } = string.Empty;
 
         public ModsConfigFile Mods { get; set; } = new ModsConfigFile();
 

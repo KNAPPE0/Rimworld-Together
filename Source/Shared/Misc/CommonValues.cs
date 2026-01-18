698e3c5f11859c4b14ffb545c59b877fca059d7b
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-01-18 10:50:34 +0100

    Update CommonValues.cs

--- Source/Shared/Misc/CommonValues.cs
+++ Source/Shared/Misc/CommonValues.cs
@@ -2,13 +2,15 @@ namespace Shared
 {
     public static class CommonValues
     {
-        public static readonly string ExecutableVersion = "dev";
+        public static readonly string MasterServer = "https://rimworldtogether.eragon.dev";
+
+        public static readonly string ExecutableVersion = "26.1.18.1";
 
         public static readonly string DefaultSaveFormat = ".json";
 
         public static readonly string TempSaveFormat = ".temp";
 
-        public static readonly string CompressedSaveFormat  = ".zip";
+        public static readonly string CompressedSaveFormat = ".zip";
 
         public static string ServerUsersPath { get; set; } = string.Empty;
 

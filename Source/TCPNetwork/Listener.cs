abc20fb0338fc8bb20761d9bc25b9faf4852315b
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-03-04 14:02:28 +0100

    Added latency display

--- Source/TCPNetwork/Listener.cs
+++ Source/TCPNetwork/Listener.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using TCPNetwork.Files.Client;
+using TCPNetwork.Misc;
 using TCPNetwork.Packets;
 using static Shared.CommonEnumerators;
 
@@ -70,6 +71,7 @@ namespace TCPNetwork
 
                     if (Stream.DataAvailable)
                     {
+                        // Read packet header
                         Stream.Read(headerBuffer, 0, sizeof(PacketHeader));
                         PacketHeader header = (PacketHeader)headerBuffer[0];
 
@@ -118,8 +120,6 @@ namespace TCPNetwork
                 {
                     Thread.Sleep(1);
 
-                    Ruleset.OnWrite?.Invoke(TargetClient);
-
                     if (PacketQueue.Count > 0)
                     {
                         if (!PacketQueue.TryDequeue(out KeyValuePair<byte, byte[]> packetData))
@@ -138,10 +138,10 @@ namespace TCPNetwork
                             Printer.Message($"[Packet] Sent packet > {(PacketHeader)packetData.Key}", LogImportanceMode.Verbose);
                         else
                             Printer.Message($"[Packet] > Sent packet {(PacketHeader)packetData.Key}", LogImportanceMode.Extreme);
-                    }
 
-                    if (IsDisconnecting)
-                        Disconnect();
+                        // Execute after writing
+                        Ruleset.OnWrite?.Invoke(TargetClient);
+                    }
                 }
             }
             catch (Exception e)

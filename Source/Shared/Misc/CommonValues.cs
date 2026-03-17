4069636b09899b2192a60d80ebad90add1ee7574
Nova <159603018+Byte-Nova@users.noreply.github.com>
2026-03-17 17:11:59 +0100

    Cleaning

--- Source/Shared/Misc/CommonValues.cs
+++ Source/Shared/Misc/CommonValues.cs
@@ -4,13 +4,13 @@ namespace Shared
     {
         public static readonly string MasterServer = "https://rimworldtogether.eragon.dev";
 
-        public static readonly string ExecutableVersion = "26.3.20.2 KMH";
+        public static string ExecutableVersion { get; set; } = "26.3.20.2 KMH";
 
-        public static readonly string DefaultSaveFormat = ".json";
+        public static string DefaultSaveFormat { get; set; } = ".json";
 
-        public static readonly string TempSaveFormat = ".temp";
+        public static string TempSaveFormat { get; set; } = ".temp";
 
-        public static readonly string CompressedSaveFormat = ".zip";
+        public static string CompressedSaveFormat { get; set; } = ".zip";
 
         public static string ServerUsersPath { get; set; } = string.Empty;
 
